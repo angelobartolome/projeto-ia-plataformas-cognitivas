7649e911069b08c756ba442cd1b06cd1c63ea39a
AlexandraPollo <100226347+AlexandraPollo@users.noreply.github.com>
2023-02-17 10:33:54 -0300

    Add files via upload

---
+++ train/train_deep.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import numpy as np 
+import matplotlib.pyplot as plt
+import sklearn
+from sklearn.neural_network import MLPClassifier
+import joblib
+from sklearn.model_selection import train_test_split
+
+if __name__ == "__main__":
+    # Carrega os dados
+    mydf = pd.read_csv('./data.csv')
+
+    targetcol = 'Status'
+    independentcols = ['income', 'age', 'year', 'Credit_Score']
+
+    # Remove rows with missing values
+    mydf = mydf.dropna(subset=independentcols)
+
+    mydf['Gender'] = mydf['Gender'].str.lower()
+
+    y = mydf[targetcol]
+    ages = ['<25', '25-34', '35-44', '45-54', '55-64', '65-74', '>74']
+
+    ages_dict = {ages[i]: i for i in range(len(ages))}
+    mydf['age'] = mydf['age'].map(ages_dict)
+
+    X = mydf[independentcols]
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.30, random_state=10)
+
+    mlpc = MLPClassifier(hidden_layer_sizes=(8,8,8), activation='relu', solver='adam', max_iter=500)
+    clf = mlpc
+    clf.fit(X=X_train[independentcols], y=y_train)
+    clf.independentcols = independentcols
+    clf_acuracia = clf.score(X=X_test[independentcols], y=y_test)
+    print("Modelo 01 (classificador), criado com acurácia de: [{0}]".format(
+        clf_acuracia))
+
+    # Salva ambos os modelos
+    joblib.dump(clf, './modelo01.joblib')
+    print("Modelo 01 (classificador) salvo com sucesso.")
+
+    pass
